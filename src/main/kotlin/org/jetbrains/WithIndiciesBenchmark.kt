81a9f8c975fd4cd1f4e044f75eccd857a7ccc917
Ilya Ryzhenkov <orangy@jetbrains.com>
2014-11-06 19:51:49 +0300

    Add withIndicies benchmark

---
+++ src/main/kotlin/org/jetbrains/WithIndiciesBenchmark.kt
@@ -0,0 +1,44 @@
+package org.jetbrains
+
+import org.openjdk.jmh.annotations.*
+import java.util.concurrent.*
+import java.util.ArrayList
+import org.openjdk.jmh.infra.Blackhole
+
+
+BenchmarkMode(Mode.AverageTime)
+OutputTimeUnit(TimeUnit.NANOSECONDS)
+open class WithIndiciesBenchmark : SizedBenchmark() {
+    private var _data: ArrayList<Value>? = null
+    val data: ArrayList<Value>
+        get() = _data!!
+
+    Setup fun setup() {
+        val list = ArrayList<Value>(size)
+        for (n in classValues(size))
+            list.add(n)
+        _data = list
+    }
+
+    CompilerControl(CompilerControl.Mode.DONT_INLINE)
+    Benchmark fun withIndicies(bh: Blackhole) {
+        for ((index, value) in data.withIndices()) {
+            if (filterLoad(value)) {
+                bh.consume(index)
+                bh.consume(value)
+            }
+        }
+    }
+
+    CompilerControl(CompilerControl.Mode.DONT_INLINE)
+    Benchmark fun withIndiciesManual(bh: Blackhole) {
+        var index = 0
+        for (value in data) {
+            if (filterLoad(value)) {
+                bh.consume(index)
+                bh.consume(value)
+            }
+            index++
+        }
+    }
+}
