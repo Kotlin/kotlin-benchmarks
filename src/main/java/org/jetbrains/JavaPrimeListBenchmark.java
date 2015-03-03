22377d46b56439f46fc43e75d9d723ee053ac3dc
Mikhail Glukhikh <Mikhail.Glukhikh@jetbrains.com>
2015-03-03 11:06:05 +0300

    New benchmarks for linked lists

---
+++ src/main/java/org/jetbrains/JavaPrimeListBenchmark.java
@@ -0,0 +1,63 @@
+package org.jetbrains;
+
+import org.jetbrains.SizedBenchmark;
+import org.openjdk.jmh.annotations.*;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * This class tests linked list performance
+ * using prime number calculation algorithms
+ *
+ * @author Mikhail Glukhikh
+ */
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+public class JavaPrimeListBenchmark extends SizedBenchmark {
+
+    private List<Integer> primes = new LinkedList<Integer>();
+
+    @Benchmark
+    @CompilerControl(CompilerControl.Mode.DONT_INLINE)
+    public void calcDirect() {
+        primes.clear();
+        primes.add(2);
+        final int size = getSize();
+        for (int i=3; i<=size; i+=2) {
+            boolean simple = true;
+            for (int prime: primes) {
+                if (prime * prime > i)
+                    break;
+                if (i % prime == 0) {
+                    simple = false;
+                    break;
+                }
+            }
+            if (simple)
+                primes.add(i);
+        }
+    }
+
+    @Benchmark
+    @CompilerControl(CompilerControl.Mode.DONT_INLINE)
+    public void calcEratosthenes() {
+        primes.clear();
+        final int size = getSize();
+        for (int i=2; i<=size; i++)
+            primes.add(i);
+        for (int i=0; i<primes.size(); i++) {
+            final int divisor = primes.get(i);
+            final Iterator<Integer> it = primes.iterator();
+            while (it.hasNext()) {
+                final int num = it.next();
+                if (num > divisor && num % divisor == 0)
+                    it.remove();
+            }
+        }
+    }
+
+}
---
+++ src/main/kotlin/org/jetbrains/PrimeListBenchmark.kt
@@ -0,0 +1,64 @@
+package org.jetbrains
+
+import org.openjdk.jmh.annotations.*
+import java.util.ArrayList
+import java.util.LinkedList
+import java.util.concurrent.TimeUnit
+
+/**
+ * Removes all elements from the list for which a given predicate is true
+ * @predicate a given predicate
+ */
+fun <T> MutableList<T>.removeAll(predicate: (T) -> Boolean) {
+    val it = this.listIterator()
+    while (it.hasNext()) {
+        val curr = it.next()
+        if (predicate(curr))
+            it.remove()
+    }
+}
+
+/**
+ * This class tests linked list performance
+ * using prime number calculation algorithms
+ *
+ * @author Mikhail Glukhikh
+ */
+BenchmarkMode(Mode.AverageTime)
+OutputTimeUnit(TimeUnit.NANOSECONDS)
+open class PrimeListBenchmark: SizedBenchmark() {
+    private var primes: MutableList<Int> = LinkedList()
+
+    CompilerControl(CompilerControl.Mode.DONT_INLINE)
+    Benchmark fun calcDirect() {
+        primes.clear()
+        primes.add(2)
+        var i = 3
+        while (i <= size) {
+            var simple = true
+            for (prime in primes) {
+                if (prime * prime > i)
+                    break
+                if (i % prime == 0) {
+                    simple = false
+                    break
+                }
+            }
+            if (simple)
+                primes.add(i)
+            i += 2
+        }
+    }
+
+    CompilerControl(CompilerControl.Mode.DONT_INLINE)
+    Benchmark fun calcEratosthenes() {
+        primes.clear()
+        primes.addAll(2..size)
+        var i = 0
+        while (i < primes.size()) {
+            val divisor = primes[i]
+            primes.removeAll { it -> it > divisor && it % divisor == 0 }
+            i++
+        }
+    }
+}
