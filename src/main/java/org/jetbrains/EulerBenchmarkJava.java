ae366e9085c5d46ad37b2ec67bf2060c4248c33a
Mikhail Glukhikh <Mikhail.Glukhikh@jetbrains.com>
2015-03-05 17:12:14 +0300

    Additional benchmarks based on Euler project

--- src/main/java/org/jetbrains/EulerBenchmarkJava.java
+++ src/main/java/org/jetbrains/EulerBenchmarkJava.java
@@ -2,6 +2,8 @@ package org.jetbrains;
 
 import org.openjdk.jmh.annotations.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -64,5 +66,77 @@ public class EulerBenchmarkJava extends SizedBenchmark {
             }
         }
         return -1;
+    }
+
+    private String veryLongNumber =
+        "73167176531330624919225119674426574742355349194934" +
+        "96983520312774506326239578318016984801869478851843" +
+        "85861560789112949495459501737958331952853208805511" +
+        "12540698747158523863050715693290963295227443043557" +
+        "66896648950445244523161731856403098711121722383113" +
+        "62229893423380308135336276614282806444486645238749" +
+        "30358907296290491560440772390713810515859307960866" +
+        "70172427121883998797908792274921901699720888093776" +
+        "65727333001053367881220235421809751254540594752243" +
+        "52584907711670556013604839586446706324415722155397" +
+        "53697817977846174064955149290862569321978468622482" +
+        "83972241375657056057490261407972968652414535100474" +
+        "82166370484403199890008895243450658541227588666881" +
+        "16427171479924442928230863465674813919123162824586" +
+        "17866458359124566529476545682848912883142607690042" +
+        "24219022671055626321111109370544217506941658960408" +
+        "07198403850962455444362981230987879927244284909188" +
+        "84580156166097919133875499200524063689912560717606" +
+        "05886116467109405077541002256983155200055935729725" +
+        "71636269561882670428252483600823257530420752963450";
+
+    @Benchmark
+    @CompilerControl(CompilerControl.Mode.DONT_INLINE)
+    public long problem8() {
+        final int productSize;
+        if (getSize() <= 10)
+            productSize = 4;
+        else if (getSize() <= 1000)
+            productSize = 8;
+        else
+            productSize = 13;
+        List<Integer> digits = new ArrayList<Integer>();
+        for (char digit: veryLongNumber.toCharArray()) {
+            if (digit >= '0' && digit <= '9') {
+                digits.add(Character.digit(digit, 10));
+            }
+        }
+        long largest = 0L;
+        for (int i=0; i<digits.size()-productSize; i++) {
+            long product = 1L;
+            for (int j=0; j<productSize; j++) {
+                product *= digits.get(i+j);
+            }
+            largest = Math.max(product, largest);
+        }
+        return largest;
+    }
+
+    @Benchmark
+    @CompilerControl(CompilerControl.Mode.DONT_INLINE)
+    public long problem9() {
+        final int size = getSize();
+        for (int c=size/3; c<=size-3; c++) {
+            long c2 = (long)c * (long)c;
+            for (int b = (size-c)/2; b<=c-1; b++) {
+                if (b+c >= size)
+                    break;
+                int a = size - b - c;
+                if (a >= b)
+                    continue;
+                long b2 = (long)b * (long)b;
+                long a2 = (long)a * (long)a;
+                if (c2 == b2 + a2) {
+                    System.out.println("" + a + "^2 + " + b + "^2 = " + c + "^2");
+                    return (long) a * (long) b * (long) c;
+                }
+            }
+        }
+        return -1L;
     }
 }
--- src/main/kotlin/org/jetbrains/EulerBenchmark.kt
+++ src/main/kotlin/org/jetbrains/EulerBenchmark.kt
@@ -73,4 +73,72 @@ open public class EulerBenchmark : SizedBenchmark() {
 //        val (multiplier, multiplicand, product) = result
 //        return product
     }
+
+    private val veryLongNumber = """
+        73167176531330624919225119674426574742355349194934
+        96983520312774506326239578318016984801869478851843
+        85861560789112949495459501737958331952853208805511
+        12540698747158523863050715693290963295227443043557
+        66896648950445244523161731856403098711121722383113
+        62229893423380308135336276614282806444486645238749
+        30358907296290491560440772390713810515859307960866
+        70172427121883998797908792274921901699720888093776
+        65727333001053367881220235421809751254540594752243
+        52584907711670556013604839586446706324415722155397
+        53697817977846174064955149290862569321978468622482
+        83972241375657056057490261407972968652414535100474
+        82166370484403199890008895243450658541227588666881
+        16427171479924442928230863465674813919123162824586
+        17866458359124566529476545682848912883142607690042
+        24219022671055626321111109370544217506941658960408
+        07198403850962455444362981230987879927244284909188
+        84580156166097919133875499200524063689912560717606
+        05886116467109405077541002256983155200055935729725
+        71636269561882670428252483600823257530420752963450
+    """
+
+    CompilerControl(CompilerControl.Mode.DONT_INLINE)
+    Benchmark fun problem8(): Long {
+        val productSize = when(size) {
+            in 1..10 -> 4
+            in 11..1000 -> 8
+            else -> 13
+        }
+        val digits: MutableList<Int> = ArrayList<Int>()
+        for (digit in veryLongNumber) {
+            if (digit in '0'..'9') {
+                digits.add(digit.toInt() - '0'.toInt())
+            }
+        }
+        var largest = 0L
+        for (i in 0..digits.size()-productSize-1) {
+            var product = 1L
+            for (j in 0..productSize-1) {
+                product *= digits[i+j]
+            }
+            largest = max(product, largest)
+        }
+        return largest
+    }
+
+    CompilerControl(CompilerControl.Mode.DONT_INLINE)
+    Benchmark fun problem9(): Long {
+        for (c in size/3..size-3) {
+            val c2 = c.toLong() * c.toLong()
+            for (b in (size-c)/2..c-1) {
+                if (b+c >= size)
+                    break
+                val a = size - b - c
+                if (a >= b)
+                    continue
+                val b2 = b.toLong() * b.toLong()
+                val a2 = a.toLong() * a.toLong()
+                if (c2 == b2 + a2) {
+                    println("$a^2 + $b^2 = $c^2")
+                    return a.toLong() * b.toLong() * c.toLong()
+                }
+            }
+        }
+        return -1L
+    }
 }
