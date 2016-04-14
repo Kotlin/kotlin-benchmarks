65b85ab8b71cfcee0cc3845d16b3259186aea648
Dmitry Jemerov <yole@jetbrains.com>
2016-04-14 18:12:09 +0200

    benchmark for various forms of loops

---
+++ src/main/java/org/jetbrains/LoopBenchmarkJava.java
@@ -0,0 +1,54 @@
+package org.jetbrains;
+
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.infra.Blackhole;
+
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author yole
+ */
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+public class LoopBenchmarkJava extends SizedBenchmark {
+  public ArrayList<Value> arrayList;
+  public Value[] array;
+
+  @Setup
+  public void setUp() {
+    arrayList = new ArrayList<Value>();
+    for (Value item : DataKt.classValues(getSize())) {
+      arrayList.add(item);
+    }
+    array = arrayList.toArray(new Value[0]);
+  }
+
+  @Benchmark
+  public void arrayForLoop(Blackhole bh) {
+    for (int i=0; i<array.length; i++) {
+      bh.consume(array[i]);
+    }
+  }
+
+  @Benchmark
+  public void arrayForeachLoop(Blackhole bh) {
+    for (Value v: array) {
+      bh.consume(v);
+    }
+  }
+
+  @Benchmark
+  public void arrayListForLoop(Blackhole bh) {
+    for (int i=0; i<arrayList.size(); i++) {
+      bh.consume(arrayList.get(i));
+    }
+  }
+
+  @Benchmark
+  public void arrayListForeachLoop(Blackhole bh) {
+    for (Value v: arrayList) {
+      bh.consume(v);
+    }
+  }
+}
---
+++ src/main/kotlin/org/jetbrains/LoopBenchmark.kt
@@ -0,0 +1,57 @@
+package org.jetbrains
+
+import org.openjdk.jmh.annotations.*
+import org.openjdk.jmh.infra.Blackhole
+import java.util.*
+import java.util.concurrent.TimeUnit
+
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+open class LoopBenchmark: SizedBenchmark() {
+    lateinit var arrayList: List<Value>
+    lateinit var array: Array<Value>
+
+    @Setup
+    fun setup() {
+        val list = ArrayList<Value>(size)
+        for (n in classValues(size))
+            list.add(n)
+        arrayList = list
+        array = list.toTypedArray()
+    }
+
+    @Benchmark fun arrayLoop(bh: Blackhole) {
+        for (x in array) {
+            bh.consume(x)
+        }
+    }
+
+    @Benchmark fun arrayIndexLoop(bh: Blackhole) {
+        for (i in array.indices) {
+            bh.consume(array[i])
+        }
+    }
+
+    @Benchmark fun arrayListLoop(bh: Blackhole) {
+        for (x in arrayList) {
+            bh.consume(x)
+        }
+    }
+
+    @Benchmark fun arrayWhileLoop(bh: Blackhole) {
+        var i = 0
+        val s = array.size
+        while (i < s) {
+            bh.consume(array[i])
+            i++
+        }
+    }
+
+    @Benchmark fun arrayForeachLoop(bh: Blackhole) {
+        array.forEach { bh.consume(it) }
+    }
+
+    @Benchmark fun arrayListForeachLoop(bh: Blackhole) {
+        arrayList.forEach { bh.consume(it) }
+    }
+}
