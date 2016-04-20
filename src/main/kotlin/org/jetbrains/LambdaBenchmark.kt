587ff11b4d152e672bdf8c4bc565f3742b4ee538
Dmitry Jemerov <yole@jetbrains.com>
2016-04-20 19:35:35 +0200

    capturing lambda with non-inline run function

--- src/main/kotlin/org/jetbrains/LambdaBenchmark.kt
+++ src/main/kotlin/org/jetbrains/LambdaBenchmark.kt
@@ -11,6 +11,9 @@ var globalAddendum = 0
 open class LambdaBenchmark : SizedBenchmark() {
     private fun <T> runLambda(x: () -> T): T = x()
 
+    @CompilerControl(CompilerControl.Mode.DONT_INLINE)
+    private fun <T> runLambdaNoInline(x: () -> T): T = x()
+
     @Setup fun init() {
         globalAddendum = ThreadLocalRandom.current().nextInt(20)
     }
@@ -28,6 +31,15 @@ open class LambdaBenchmark : SizedBenchmark() {
         var x: Int = 0
         for (i in 0..size) {
             x += runLambda { addendum }
+        }
+        return x
+    }
+
+    @Benchmark fun capturingLambdaNoInline(): Int {
+        val addendum = globalAddendum + 1
+        var x: Int = 0
+        for (i in 0..size) {
+            x += runLambdaNoInline { addendum }
         }
         return x
     }
